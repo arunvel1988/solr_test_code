0fcf2c895569307e56f7946c602eec9dd7a2a98d
Arunvel Arunachalam <30715441+arunvel1988@users.noreply.github.com>
2020-02-12 19:21:10 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 #Simple script to check if solr is running or not
 #Enter IP Address or FQDN of Solr Host
-solr_host="localhost:8984/solr/"
+solr_host="http://localhost:8984/solr/"
 
 #Using curl to hit FQDN and check response code of  Solr Server
 resp=$(curl --write-out %{http_code} --silent --output /dev/null "$solr")
