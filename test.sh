fa035e92de8fe18c09461ae5defb662aad502e37
Arunvel Arunachalam <30715441+arunvel1988@users.noreply.github.com>
2020-02-12 19:20:11 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-solr="localhost:8983"
 
-resp=$(curl --write-out %{http_code} --silent --output /dev/null "http://$solr/")
-echo $resp
+#Simple script to check if solr is running or not
+#Enter IP Address or FQDN of Solr Host
+solr_host="localhost:8984/solr/"
 
+#Using curl to hit FQDN and check response code of  Solr Server
+resp=$(curl --write-out %{http_code} --silent --output /dev/null "$solr")
+#Saving code in resp variable
+echo $resp                                                      
 
+#Normal if else condition to check if response code is 200 or not
 if [ "$resp" = "200" ]
 then
   echo "Solr Server is Up"
 else
-  exit 1
+  echo "Solr server not running"
 
 
 fi
